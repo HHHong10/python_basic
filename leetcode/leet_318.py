6cbc0890b1472d548d5235a3dcc621ff128451fd
HHHong10 <jymsungmi@naver.com>
2021-11-11 17:09:47 +0900

    Maximum Product of Word Lengths

--- leetcode/leet_318.py
+++ leetcode/leet_318.py
@@ -1,1 +1,15 @@
-# string007
+def maxProduct(words):
+    def isUnique(s1, s2):
+        for x in s1:
+            if s2.find(x) != -1:
+                return False
+        return True
+    answer = 0
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            if isUnique(words[i], words[j]):
+                temp = len(words[i]) * len(words[j])
+                answer = max(answer, temp)
+    return answer
+
+print(maxProduct(["abcw","baz","foo","bar","xtfn","abcdef"]))
