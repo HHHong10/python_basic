2ba89debf99cc1e4788e68502bb51f061e4b537c
HHHong10 <jymsungmi@naver.com>
2021-11-11 17:11:20 +0900

    string

--- leetcode/leet_1832.py
+++ leetcode/leet_1832.py
@@ -1,12 +1,9 @@
 import collections
 def solution(sentence):
-    # answer = True
-    # stringHash = {}
     # stringHash = collections.Counter(sentence)
     # if len(stringHash.keys()) != 26:
-    #     answer = False
-    #     return answer
-    # return answer
+    #     return False
+    # return True
     return len(collections.Counter(sentence).keys()) == 26
 
 print(solution("thequickbrownfoxjumpsoverthelazydog"))
